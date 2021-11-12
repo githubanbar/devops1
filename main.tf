2f4780a534edc753aeac47a2c9508f89b3a1c051
devopsuser <anbartcssymcor@gmail.com>
2021-11-12 00:38:12 -0500

    updated file

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_eks_cluster" "" {
-  name =""
-  role_arn=""
+  name     = ""
+  role_arn = ""
   vpc_config {
     subnet_ids = []
   }
 }
 
-resource "aws_chime_voice_connector" ""{
-  name =""
-  require_encryption =false
-}
+
+
+
+
